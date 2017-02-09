c8e7596d52146e989a5f7f5fff02faaa0b0631ff
SamDing1999 <apple@applematoair.attlocal.net>
2017-02-08 21:18:26 -0800

    0208

---
+++ Ticket/src/StudentAdvancedTicket.java
@@ -0,0 +1,16 @@
+
+public class StudentAdvancedTicket extends AdvancedTicket{
+	
+	public StudentAdvancedTicket(int ticketNumber, int daysInAdvance){
+		super(ticketNumber, daysInAdvance);
+		 if(daysInAdvance >= limit){
+		    	super.price = 15;
+		    }else{
+		    	super.price = 20;
+		    }
+	}
+	
+	public String toString(){
+		return super.toString() ;
+	}
+}
---
+++ Ticket/src/Ticket.java
@@ -0,0 +1,17 @@
+
+public class Ticket {
+	int price;
+	private int ticketNum;
+	
+	public Ticket(int ticketNum){
+		this.ticketNum = ticketNum;
+	}
+	
+	public int getPrice(){
+		return this.price;
+	}
+	
+	public String toString(){
+	    return ( "Number: " + this.ticketNum + ", Price: " + this.getPrice());
+	}
+}
