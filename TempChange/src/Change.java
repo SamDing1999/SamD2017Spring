1d1efce2a85c7a4bf9fc4d9f91936fe2e6e0b465
SamDing1999 <apple@applematoair.attlocal.net>
2017-02-22 22:26:50 -0800

    435

--- TempChange/src/Change.java
+++ TempChange/src/Change.java
@@ -3,13 +3,18 @@ import java.util.*;
 
 public class Change {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(new File("weather"));
+	public static void main(String[] args) throws FileNotFoundException{
+		File weather = new File("./resources/weather.txt");
+		System.out.println(weather.canRead()+ "");
+		//new File("./resources/weather.txt")
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please record the temperature");
 		double first = input.nextDouble();
 		for(int i = 0; i< 7; i++){
 			System.out.println("Please record the temperature");
 			double temp = input.nextDouble();
-			
+			System.out.println(first + " to "+ temp + ", change = " + (double)(temp-first));
+			first = temp;
 		}
 
 	}
