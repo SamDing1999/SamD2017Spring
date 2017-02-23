bfc454c462901784a0af8eac59325faadcf1800a
SamDing1999 <apple@applematoair.attlocal.net>
2017-02-22 19:31:20 -0800

    9090

---
+++ TempChange/src/Change.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Change {
+
+	public static void main(String[] args) {
+		File change = new File();
+		Scanner input = new Scanner(new File("change"));
+		double first = input.nextDouble();
+		for(int i = 0; i< 7; i++){
+			System.out.println("Please record the temperature");
+			double temp = input.nextDouble();
+			
+		}
+
+	}
+
+}
